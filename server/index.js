14263ddbad9002322151e407867f17b1eb7d1093
virtus <AleksySi2509@protonmail.com>
2023-05-23 20:24:22 +0100

    Added main feed & specific user feed routes

--- server/index.js
+++ server/index.js
@@ -8,9 +8,13 @@ import helmet from "helmet";
 import morgan from "morgan";
 import path from "path";
 import { fileURLToPath } from "url";
-import authRoutes from "./routes/auth.js"
-import { register } from "./controllers/auth.js"
-import  userRoutes  from "./routes/users.js"
+import authRoutes from "./routes/auth.js";
+import { register } from "./controllers/auth.js";
+import  userRoutes  from "./routes/users.js";
+import  postRoutes  from "./routes/posts.js";
+import { verifyToken } from "./middleware/auth.js";
+import {  createPost} from "./controllers/posts.js"
+
 
 
 /* CONFIGURATIONS */
@@ -40,10 +44,12 @@ const upload = multer({ storage });
 
 /* ROUTES WITH FILES*/
 app.post("/auth/register", upload.single("picture"), register);
+app.post("/posts", verifyToken, upload.single("picture"), createPost);
 
 /* ROUTES */
 app.use("/auth", authRoutes);
 app.use("/users", userRoutes);
+app.use("posts", postRoutes);
 
 
 /* MONGOOSE SETUP*/
