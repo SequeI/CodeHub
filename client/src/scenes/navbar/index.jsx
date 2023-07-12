58fce10ff15c5d9360f183b10c956815d50f6de2
virtus <AleksySi2509@protonmail.com>
2023-07-12 15:30:45 +0100

    Added desktop nav

--- client/src/scenes/navbar/index.jsx
+++ client/src/scenes/navbar/index.jsx
@@ -58,9 +58,44 @@ const Navbar = () => {
 
         { /* DESKTOP NAV */}
         {isNonMobileScreens ? (
-            <FlexBetween></FlexBetween>
+            <FlexBetween gap="2rem">
+                <IconButton onClick={() => dispatch(setMode())}>
+                    {theme.palette.mode === "dark" ? (
+                        <DarkMode sx={{ fontSize: "25px" }} />
+                    ) : (
+                        <LightMode sx={{ color: dark, fontSize: "25px" }} />
+                    )}
+                </IconButton>
+                <Message sx={{ fontSize: "25px" }} />
+                <Notifications sx={{ fontSize: "25px" }} />
+                <Help sx={{ fontSize: "25px" }} />
+                <FormControl variant="standard" value={fullName}>
+                    <Select value={fullName} sx={{
+                        backgroundColor: neutralLight,
+                        width: "150px",
+                        borderRadius: "0.25rem",
+                        p: "0.25rem 1rem",
+                        "& .MuiSvgIcon-root": {
+                            pr: "0.25rem",
+                            width: "3rem"
+                        },
+                        "& .MuiSelect-select:focus": {
+                            backgroundColor: neutralLight
+                        }
+                    }}
+                        input={<InputBase />}
+                    >
+                        <MenuItem value={fullName}>
+                            <Typography>{fullName}</Typography>
+                        </MenuItem>
+                        <MenuItem onClick={() => dispatch(setLogout())}>Log Out</MenuItem>
+                    </Select>
+                </FormControl>
+            </FlexBetween>
         ) : (
-            <IconButton></IconButton>
+            <IconButton onClick={() => setIsMobileMenuToggled(!isMobileMenuToggled)}>
+                <Menu />
+            </IconButton>
         )}
     </FlexBetween>;
 };
