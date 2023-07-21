e38f092ff6cbc98a7dcf3e1747f24f70459cfbfa
virtus <AleksySi2509@protonmail.com>
2023-07-21 09:44:21 +0100

    Added post functionality for widget

--- client/src/scenes/homePage/index.jsx
+++ client/src/scenes/homePage/index.jsx
@@ -2,6 +2,7 @@ import { Box, useMediaQuery } from "@mui/material";
 import { useSelector } from "react-redux";
 import Navbar from "scenes/navbar";
 import UserWidget from "scenes/widgets/UserWidget";
+import MyPostWidget from "scenes/widgets/MyPostWidget";
 
 
 const HomePage = () => {
@@ -14,9 +15,11 @@ const HomePage = () => {
             <Box flexBasis={isNonMobileScreens ? "26%" : undefined}>
                 <UserWidget userID={_id} picturePath={picturePath} />
             </Box>
-            <Box flexBasis={isNonMobileScreens ? "42%" : undefined} mt={isNonMobileScreens ? undefined : "2rem"}></Box>
+            <Box flexBasis={isNonMobileScreens ? "42%" : undefined} mt={isNonMobileScreens ? undefined : "2rem"}>
+                <MyPostWidget picturePath={picturePath} />
+            </Box>
             {isNonMobileScreens && <Box flexBasis="26%" ></Box >}
-                </Box>
+        </Box>
     </Box>
 };
 
